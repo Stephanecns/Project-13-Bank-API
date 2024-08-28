d43ac47de67ba2d7df47cb1cdf766f08fbcb9325
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2024-08-28 19:07:32 +0200

    Ajout divers

--- client/src/components/SignInPage.js
+++ client/src/components/SignInPage.js
@@ -1,26 +1,56 @@
-import React from 'react';
+// src/components/SignInPage.js
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { loginUser } from '../redux/actions/authActions';
 
 const SignInPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const userDetails = useSelector((state) => state.user.userDetails);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    await dispatch(loginUser({ email, password }));
+  };
+
+  // Rediriger vers le profil utilisateur après la mise à jour des informations utilisateur
+  useEffect(() => {
+    if (isAuthenticated && userDetails.firstName) {
+      navigate('/user-profile');
+    }
+  }, [isAuthenticated, userDetails, navigate]);
+
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form>
+        <form onSubmit={handleLogin}>
           <div className="input-wrapper">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
-          <div className="input-remember">
-            <input type="checkbox" id="remember-me" />
-            <label htmlFor="remember-me">Remember me</label>
-          </div>
-          {/* Utilisez un bouton pour gérer la soumission du formulaire */}
-          <button className="sign-in-button" type="submit">Sign In</button>
+          <button className="sign-in-button" type="submit">
+            Sign In
+          </button>
         </form>
       </section>
     </main>
--- client/src/components/UserProfilePage.js
+++ client/src/components/UserProfilePage.js
@@ -1,39 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUserProfile } from '../redux/actions/authActions';
 import Account from './Account';
 
-const accounts = [
-  {
-    title: 'Argent Bank Checking (x8349)',
-    amount: '2,082.79',
-    description: 'Available Balance',
-  },
-  {
-    title: 'Argent Bank Savings (x6712)',
-    amount: '10,928.42',
-    description: 'Available Balance',
-  },
-  {
-    title: 'Argent Bank Credit Card (x8349)',
-    amount: '184.30',
-    description: 'Current Balance',
-  }
-];
+const UserProfilePage = () => {
+  const dispatch = useDispatch();
+  const userDetails = useSelector((state) => state.user.userDetails);
 
-const UserProfilePage = () => (
-  <main className="main bg-dark">
-    <div className="header">
-      <h1>Welcome back<br />Tony Jarvis!</h1>
-      <button className="edit-button">Edit Name</button>
-    </div>
-    {accounts.map((account, index) => (
-      <Account
-        key={index}
-        title={account.title}
-        amount={account.amount}
-        description={account.description}
-      />
-    ))}
-  </main>
-);
+  const [isEditing, setIsEditing] = useState(false);
+  const [firstName, setFirstName] = useState(userDetails.firstName || '');
+  const [lastName, setLastName] = useState(userDetails.lastName || '');
+
+  const accounts = [
+    {
+      title: 'Argent Bank Checking (x8349)',
+      amount: '2,082.79',
+      description: 'Available Balance',
+    },
+    {
+      title: 'Argent Bank Savings (x6712)',
+      amount: '10,928.42',
+      description: 'Available Balance',
+    },
+    {
+      title: 'Argent Bank Credit Card (x8349)',
+      amount: '184.30',
+      description: 'Current Balance',
+    },
+  ];
+
+  const handleEditToggle = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleSave = (e) => {
+    e.preventDefault();
+    dispatch(updateUserProfile({ firstName, lastName }));
+    setIsEditing(false);
+  };
+
+  return (
+    <main className="main bg-dark">
+      <div className="header">
+        <h1>
+          Welcome back,<br /> {firstName} {lastName}!
+        </h1>
+        <button className="edit-button" onClick={handleEditToggle}>
+          {isEditing ? 'Cancel' : 'Edit Name'}
+        </button>
+      </div>
+      {isEditing ? (
+        <form onSubmit={handleSave} className="edit-form">
+          <div className="input-wrapper">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="edit-button">
+            Save
+          </button>
+        </form>
+      ) : (
+        <>
+          {accounts.map((account, index) => (
+            <Account
+              key={index}
+              title={account.title}
+              amount={account.amount}
+              description={account.description}
+            />
+          ))}
+        </>
+      )}
+    </main>
+  );
+};
 
 export default UserProfilePage;
